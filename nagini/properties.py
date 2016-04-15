91dd4bdd8ad095a006778f0041a5fbf2610c430d
Alexandr Litovchenko <zedlaa@gmail.com>
2016-04-15 17:35:15 +0300

    Improve propertries, update debian scripts

--- nagini/properties.py
+++ nagini/properties.py
@@ -1,9 +1,12 @@
 # -*- coding: utf8 -*-
 import json
 import os
+import re
 
 
 class Properties(dict):
+    FLOAT_RE = r'^\d+\.\d*$'
+
     def load(self):
         if "JOB_PROP_FILE" in os.environ:
             with open(os.environ["JOB_PROP_FILE"]) as fd:
@@ -23,9 +26,46 @@ class Properties(dict):
         else:
             print "No JOB_OUTPUT_PROP_FILE"
 
+    def get_list(self, name, val_func='auto'):
+        """Parse value with key=name and transform it to list.
+        If val_vun
+
+        :param str name:
+        :param func val_func:
+        :return:
+        """
+        value = self.get(name, '').strip().strip(',')
+        if value:
+            value_list = [v.strip() for v in value.split(',')]
+            if val_func == 'auto':
+                return [Properties._parse_value(v) for v in value_list]
+            elif val_func:
+                return [val_func(v) for v in value_list]
+            else:
+                return value_list
+        else:
+            return []
+
+    @staticmethod
+    def _parse_value(val):
+        """Return proper typed value
+
+        :param str val: value as a string
+        :rtype: str|unicode|int|float|long
+        """
+        if re.match(Properties.FLOAT_RE, val):
+            return float(val)
+        elif val.isdigit():  # auto int and long
+            return int(val)
+        else:
+            try:
+                return val.decode('utf8')
+            except UnicodeDecodeError:
+                return val
+
 
 def load_properties():
-    props = {}
+    props = Properties()
     if "JOB_PROP_FILE" in os.environ:
         with open(os.environ["JOB_PROP_FILE"]) as fd:
             for line in fd:
--- setup.py
+++ setup.py
@@ -18,15 +18,16 @@ class TestCommand(Command):
 
 setup(
     name="nagini",
-    version="0.1",
+    version="0.6",
     author="Alexandr Litovchenko",
     author_email="zedlaa@gmail.com",
     packages=["nagini", "nagini.builder"],
     scripts=["nagini-build.py", "nagini-run.py", "nagini-data.py"],
-    requires=["jinja2", "requests"],
+    requires=["jinja2", "requests", "PyYAML"],
     data_files=[
         ("/usr/share/nagini/", ["data/job-template.job.j2"]),
-        ("/usr/share/nagini/", ["data/launcher.py.j2"])
+        ("/usr/share/nagini/", ["data/launcher.py.j2"]),
+        ("/etc/", ["nagini.yml"])
     ],
     url="unknown",
     description="",
