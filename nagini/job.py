434c7bf89d824f62c60bfe24b63e56846d453e2b
Alexandr Litovchenko <zedlaa@gmail.com>
2016-04-15 17:33:01 +0300

    Using logger in job, add some base classes + using config

--- nagini/job.py
+++ nagini/job.py
@@ -1,12 +1,20 @@
 # -*- coding: utf8 -*-
 from nagini.properties import load_properties, save_properties
+from dateutil.rrule import rrule, MO, MONTHLY, WEEKLY, DAILY
+from dateutil.relativedelta import relativedelta
+from abc import ABCMeta, abstractmethod
 from nagini.utility import flatten
 from nagini.target import Target
 from os.path import join, exists
 from os import mkdir, environ
+from datetime import datetime
+from copy import deepcopy
+import logging.config
 import subprocess
+import logging
 import shutil
 import json
+import yaml
 
 
 class BaseJob(object):
@@ -15,10 +23,24 @@ class BaseJob(object):
     name = None
     retries = 0
     retry_backoff = 0
+    config = None
 
     def __init__(self):
         self.props = load_properties()
         self.env = environ.copy()
+        if exists('/etc/nagini.yml'):
+            with open('/etc/nagini.yml') as fd:
+                config = yaml.load(fd)
+                if 'logging' in config:
+                    logging.config.dictConfig(config['logging'])
+        self.logger = logging.getLogger('nagini.job.%s' %
+                                        self.__class__.__name__)
+        if 'working.dir' in self.props:
+            self.props['working.dir.nagini'] = join(self.props['working.dir'],
+                                                    'nagini_data')
+
+            with open(join(self.props['working.dir'], 'config.yml')) as fd:
+                self.config = yaml.load(fd)
 
     def requires(self):
         """Override me!"""
@@ -53,7 +75,7 @@ class BaseJob(object):
 
     def run(self):
         """Override me!"""
-        print "Nagini: this job is empty. Override run() method"
+        self.logger.error("Nagini: this job is empty. Override run() method")
 
     def on_failure(self):
         for target in flatten(self.output()):
@@ -72,29 +94,29 @@ class BaseJob(object):
         except OSError:
             pass
         self.configure()
-        print "Init props:"
-        print json.dumps(self.props, ensure_ascii=False, indent=4)
+        self.logger.info("Init props:\n" +
+                         json.dumps(self.props, ensure_ascii=False, indent=4))
         output = flatten(self.output())
         if not output:
             self._check_output_at_start = False
         try:
-            print "Nagini: start job"
+            self.logger.info("Nagini: start job")
             if self._check_output_at_start and all(t.exists() for t in output):
-                print "All targets exists at start of the job, skip job..."
+                self.logger.warning("All targets exists at start "
+                                    "of the job, skip job...")
             else:
-                print "Nagini: about to run"
+                self.logger.info('Nagini: about to execute "run" method')
                 self.run()
             for key, value in self.env.iteritems():
                 self.props["env.%s" % key] = value
             save_properties(self.props)
             if self._check_output_at_end and not all(t.exists() for t in output):
-                raise Exception(
-                    "Not all output target exists at end of the job"
-                )
+                raise Exception("Not all output target exists "
+                                "at end of the job")
             else:
                 self.on_success()
-        except BaseException:
-            print "NaginiJob: catch exception. Try on_failure()"
+        except BaseException as e:
+            self.logger.error("NaginiJob: catch exception. Try on_failure()")
             self.on_failure()
             raise
 
@@ -230,3 +252,108 @@ class MySqlQueryJob(BaseJob):
 class ClearAllJob(BaseJob):
     def run(self):
         self.clear_data_dir()
+
+
+class BaseChecker(BaseJob):
+    work_flow = None
+    work_flow_params = None
+    __metaclass__ = ABCMeta
+
+    def run(self):
+        if self.need_update():
+            self.start_work_flow()
+
+    @abstractmethod
+    def need_update(self):
+        raise NotImplementedError('You must implement "need_update" method')
+
+    def start_work_flow(self):
+        if self.work_flow is None:
+            raise ValueError('You must set "work_flow" property')
+        else:
+            self.work_flow.start(self.work_flow_params or {})
+
+
+class DataChecker(BaseChecker):
+    __metaclass__ = ABCMeta
+
+    def need_update(self):
+        if self.src_data_exists() and self.dst_data_exists():
+            self.start_work_flow()
+
+    @abstractmethod
+    def src_data_exists(self):
+        raise NotImplementedError('You must implement '
+                                  '"src_data_exists" method')
+
+    @abstractmethod
+    def dst_data_exists(self):
+        raise NotImplementedError('You must implement '
+                                  '"dst_data_exists" method')
+
+
+class IntervalDataChecker(BaseJob):
+    __metaclass__ = ABCMeta
+    check_interval = None
+    type = None
+    src = None
+    source_data_peace_delta = None
+    source_data_count = 1
+    prepared_data_pattern = None
+    work_flow = None
+    work_flow_params = None
+
+    def run(self):
+        end = None
+        start = None
+
+        if self.type == MONTHLY:
+            end = datetime.now() - relativedelta(months=1, day=1, hour=0,
+                                                 minute=0, second=0)
+            start = end - relativedelta(months=self.check_interval)
+        elif self.type == WEEKLY:
+            end = datetime.now() - relativedelta(weekday=MO(-1), hour=0,
+                                                 minute=0, second=0)
+            start = end - relativedelta(weeks=self.check_interval)
+        elif self.type == DAILY:
+            end = datetime.now() - relativedelta(hour=0, minute=0, second=0)
+            start = end - relativedelta(days=self.check_interval)
+
+        for current in rrule(self.type, start, until=end):
+            s = current
+            if self.type == MONTHLY:
+                e = s + relativedelta(months=1)
+            elif self.type == WEEKLY:
+                e = s + relativedelta(weeks=1)
+            elif self.type == DAILY:
+                e = s + relativedelta(days=1)
+
+            if self.work_flow and not self.dst_data_exists(s, e):
+                print 'Prepared data not exists for', s, e
+                if self.src_data_exists(s, e):
+                    print "Start prepare data. Start: %s, End: %s" % (s, e)
+                    self.start_work_flow(s, e)
+
+    @abstractmethod
+    def src_data_exists(self, s, e):
+        raise NotImplementedError('You must implement '
+                                  '"src_data_exists" method')
+
+    @abstractmethod
+    def dst_data_exists(self, s, e):
+        raise NotImplementedError('You must implement '
+                                  '"dst_data_exists" method')
+
+    def start_work_flow(self, s, e):
+        params = deepcopy(self.work_flow_params) or {}
+
+        if self.type == MONTHLY:
+            params["month"] = s.strftime("%Y-%m")
+        elif self.type == WEEKLY:
+            params.update({"week": s.strftime("%Y-%m-%d"),
+                           "start": s.strftime("%Y-%m-%d"),
+                           "end": e.strftime("%Y-%m-%d")})
+        elif self.type == DAILY:
+            params["day"] = s.strftime("%Y-%m-%d")
+
+        self.work_flow.start(params)
