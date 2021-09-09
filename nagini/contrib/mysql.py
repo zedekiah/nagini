0b74fb5764daa1826d7b816fc5d73512b8ca5cbc
Gleb Orlov <orlovgb@yandex-team.ru>
2021-09-09 17:31:09 +0300

    Set output_flag to output Targets

--- nagini/contrib/mysql.py
+++ nagini/contrib/mysql.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8; -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from nagini import BaseJob
+from nagini.job import BaseJob
 import subprocess
 
 
